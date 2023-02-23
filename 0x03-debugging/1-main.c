f6f250fac62f4d8d9848152f55d5f084b89a8690
Ittani Maemo <you@example.com>
2023-02-23 05:19:20 -0800

    1. Like, comment, subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+int i;
+/*while (i < 10)*/
+/*{*/
+/*putchar(i);*/
+/*}*/
+printf("Infinite loop avoided! \\o/\n");
+return (0);
+}
