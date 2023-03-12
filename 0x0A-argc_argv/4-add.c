fa0d2ed58ab5f686cecc46950b45bf9de152dd6e
Ittani Maemo <you@example.com>
2023-03-12 13:36:51 -0700

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -22,3 +22,32 @@ count++;
 }
 return (1);
 }
+/**      
+ * main - Print the name of the program   
+ * @argc: Count arguments   
+ * @argv: Arguments       
+ * Return: Always 0 (Success) 
+ */
+int main(int argc, char *argv[])  
+{
+int count;
+int str_to_int;        
+int sum = 0;    
+count = 1;     
+while (count < argc) /*Goes through the whole array*/    
+{
+if (check_num(argv[count]))     
+{
+str_to_int = atoi(argv[count]);
+sum += str_to_int;          
+}
+else
+{
+printf("Error\n"); 
+return (1);      
+}
+count++;      
+}
+printf("%d\n", sum); 
+return (0);       
+}
