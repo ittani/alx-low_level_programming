e0dd8b84d2771c73943a227c03e9e1db76cf4a9c
ittani <ittanim@gmail.com>
2023-05-04 10:58:00 -0700

    2. Add node

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *new;
+new = malloc(sizeof(listint_t));
+if (!new)
+return (NULL);
+new->n = n;
+*head = new;
+return (new);
+}
